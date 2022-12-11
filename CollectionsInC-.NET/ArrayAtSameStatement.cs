0f5ccd7b17bcc1b0f92d552945bdbc1c8947c794
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-11 11:44:36 +0530

    [Mahesh]Add:ArrayAtSameStatement

---
+++ CollectionsInC-.NET/ArrayAtSameStatement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsInC_.NET
+{
+    internal class ArrayAtSameStatement
+    {
+        public static void Array()
+        {
+            //Creating and Initializing an Array
+            //Here, the size will be decided based on the number of elements
+            int[] Numbers = { 101, 202, 203 };
+            //accessing array elements separately
+            Console.WriteLine("Accessing the Array Elements separately");
+            Console.WriteLine($"Numbers[0] = {Numbers[0]}");
+            Console.WriteLine($"Numbers[1] = {Numbers[1]}");
+            Console.WriteLine($"Numbers[2] = {Numbers[2]}");
+            Console.WriteLine("\nAccessing the Array Elements using ForEach Loop");
+            foreach (var data in Numbers)
+            {
+                Console.WriteLine(data);
+            }
+            Console.WriteLine("\nAccessing the Array Elements using For Loop");
+            for(int i=0;i<=Numbers.Length-1;i++)
+            {
+                Console.WriteLine($"Numbers[{i}] = {Numbers[i]}");
+            }
+           // Console.ReadKey();
+        }
+    }
+}
--- CollectionsInC-.NET/Program.cs
+++ CollectionsInC-.NET/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace CollectionsInC_.NET
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Collections Of Elements in C#.NET");
+                Console.WriteLine("Click the below options -\n1.ArrayAtSameStatement");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option) {
+                case 1:
+                Console.WriteLine("Creating and Initializing an Array at the Same Statement//int[] Numbers = { 101, 202, 303 };//");
+                ArrayAtSameStatement.Array();
+                        break;
+            }
+             
+            } 
+        }
+    }
+}
