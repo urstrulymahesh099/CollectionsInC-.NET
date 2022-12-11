4f64b93786b158d8ed800c93661192840112dc3e
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-11 12:18:52 +0530

    [Mahesh]Add:ArrayInitializingSeparately

---
+++ CollectionsInC-.NET/ArrayInitializingSeparately.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsInC_.NET
+{
+    internal class ArrayInitializingSeparately
+    {
+
+       public static void Array1()
+        {
+            //Creating an Integer Array with size 3
+            int[] Numbers = new int[10];
+            Numbers[9] = 400;
+            Numbers[7] = 500;
+            Numbers[6] = 600;
+            //Accessing the Array Elements Before Initialization
+            Console.WriteLine("Accessing the Array Elements");
+            for (int i = 0; i <= Numbers.Length - 1; i++)
+            {
+                Console.WriteLine($"Numbers[{i}]={Numbers[i]}");
+            }
+            Numbers[0] = 10;
+            Numbers[1] = 20;
+            Numbers[2] = 30;
+            //Accessing the Array Elements After Initialization
+            Console.WriteLine("\nAccessing the Array Elements After Initialization in foreach loop");
+            foreach (var data in Numbers)
+            {
+                Console.WriteLine(data);
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
--- CollectionsInC-.NET/Program.cs
+++ CollectionsInC-.NET/Program.cs
@@ -8,14 +8,18 @@ namespace CollectionsInC_.NET
             while (true)
             {
                 Console.WriteLine("Collections Of Elements in C#.NET");
-                Console.WriteLine("Click the below options -\n1.ArrayAtSameStatement");
+                Console.WriteLine("Click the below options -\n1.ArrayAtSameStatement\n2.ArrayInitializingSeparately");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option) {
-                case 1:
-                Console.WriteLine("Creating and Initializing an Array at the Same Statement//int[] Numbers = { 101, 202, 303 };//");
-                ArrayAtSameStatement.Array();
+                    case 1:
+                        Console.WriteLine("Creating and Initializing an Array at the Same Statement//int[] Numbers = { 101, 202, 303 };//");
+                        ArrayAtSameStatement.Array();
                         break;
-            }
+                    case 2:
+                        Console.WriteLine("Creating and Initializing an Array Separately//int[] Numbers = new int[8];//");
+                        ArrayInitializingSeparately.Array1();
+                        break;
+                }
              
             } 
         }
